3e5e93a00e2f05fe59a68efe5bd27afb5336f8d1
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-06-22 16:06:56 +0530

    Updates

---
+++ precache-manifest.dcde55d52bd4d39288f193e0a4d28500.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39d495c3478788b870fdd0188c9ba49",
+    "url": "/STI1/index.html"
+  },
+  {
+    "revision": "89cd90bd3175db8bd4fb",
+    "url": "/STI1/static/css/main.fc8fade6.chunk.css"
+  },
+  {
+    "revision": "a59b2137adfbacc99c9e",
+    "url": "/STI1/static/js/2.7094c61f.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/STI1/static/js/2.7094c61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89cd90bd3175db8bd4fb",
+    "url": "/STI1/static/js/main.2ec24949.chunk.js"
+  },
+  {
+    "revision": "6e27d8aed40d2723f4b5",
+    "url": "/STI1/static/js/runtime-main.3ab06b62.js"
+  },
+  {
+    "revision": "f6b13285b10f1425439d4a4e7424df84",
+    "url": "/STI1/static/media/B1.f6b13285.png"
+  },
+  {
+    "revision": "0f8ff573c8bb92f14213df080feae685",
+    "url": "/STI1/static/media/B2.0f8ff573.png"
+  },
+  {
+    "revision": "2c71a456f2904085d5f9bb2e526f9f11",
+    "url": "/STI1/static/media/B5a.2c71a456.png"
+  },
+  {
+    "revision": "a2fce84a66ca461926689db1260fbfb0",
+    "url": "/STI1/static/media/B5c.a2fce84a.png"
+  },
+  {
+    "revision": "777bcfd44726291d3955cd84ce341130",
+    "url": "/STI1/static/media/B7.777bcfd4.png"
+  }
+]);
