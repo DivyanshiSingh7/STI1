77ca578e7f8b21d2dd17acd9e1e174e9ee96af63
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-06-25 15:31:33 +0530

    Updates

---
+++ precache-manifest.3270b9e690b432e665b1a681d3278610.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f029983d701ff1a10c2a3b5cbc3f4b87",
+    "url": "/tools/PMDD-tool/index.html"
+  },
+  {
+    "revision": "b299b8301798c98bed2d",
+    "url": "/tools/PMDD-tool/static/css/main.67e5a04a.chunk.css"
+  },
+  {
+    "revision": "5f96d06b233f7a2635f8",
+    "url": "/tools/PMDD-tool/static/js/2.bf4e51d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tools/PMDD-tool/static/js/2.bf4e51d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b299b8301798c98bed2d",
+    "url": "/tools/PMDD-tool/static/js/main.2f71383c.chunk.js"
+  },
+  {
+    "revision": "e165aeb5f44627c1f180",
+    "url": "/tools/PMDD-tool/static/js/runtime-main.0628f531.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tools/PMDD-tool/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/STI1/precache-manifest.e8aff25f71d77fd983cc29b4e8b2e6c3.js"
+  "/tools/PMDD-tool/precache-manifest.3270b9e690b432e665b1a681d3278610.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/STI1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tools/PMDD-tool/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
